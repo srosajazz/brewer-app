f861c73b0f3447a3b4668035b95944f5e2d127e4
Sergio Rosa <scarlosjazz@gmail.com>
2020-03-30 18:53:07 -0400

    form Validation Bean-Validation-Hibernate-Validator

--- src/main/java/com/sergiorosa/brewer/controller/BeersController.java
+++ src/main/java/com/sergiorosa/brewer/controller/BeersController.java
@@ -1,8 +1,29 @@
 package com.sergiorosa.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sergiorosa.brewer.model.Beer;
 
 @Controller
 public class BeersController {
+	
+	@RequestMapping("/beer/test")
+	public String test() {
+		return "beer/BeerRegistration";
+	}
 
+	@RequestMapping(value = "/beer/test", method = RequestMethod.POST)
+	public String registration(@Valid Beer beer, BindingResult result) {
+		if(result.hasErrors()) {
+			System.out.print(">>> ERROR!");
+		}
+		System.out.println(">>> sku: " + beer.getSku());
+		System.out.println(">>>> register!");
+		return "beer/BeerRegistration";
+	}
 }
